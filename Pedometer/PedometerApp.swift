d66182cd129b6f95fe6974e5750674e26075505e
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2024-07-21 15:43:50 -0400

    Initial Commit

---
+++ Pedometer/PedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedometerApp.swift
+//  Pedometer
+//
+//  Created by Gaurav Bhambhani on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
